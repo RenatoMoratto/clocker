53916f8d276ffc63bc6142e502b31e4e631a7404
Facesoccer <80411445+RenatoMoratto@users.noreply.github.com>
2021-08-26 11:23:22 -0300

    Add yup and formik to form

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import { Formik, useFormik } from 'formik';
+import * as yup from 'yup';
+
 import {
   Container,
   Box,
@@ -12,6 +15,25 @@ import {
 import { Logo } from '../components/Logo';
 
 export default function Home() {
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('E-mail inválido')
+      .required('Preenchimento obrigatório'),
+    password: yup.string().required('Preenchimento obrigatório'),
+    username: yup.string().required('Preenchimento obrigatório'),
+  });
+
+  const formik = useFormik({
+    onSubmit: () => {},
+    validationSchema,
+    initialValues: {
+      email: '',
+      usename: '',
+      password: '',
+    },
+  });
+
   return (
     <Container p={4} centerContent>
       <Logo />
@@ -21,21 +43,53 @@ export default function Home() {
       <Box>
         <FormControl id="email" p={4} isRequired>
           <FormLabel>Email</FormLabel>
-          <Input type="email" />
+          <Input
+            type="email"
+            values={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && (
+            <FormHelperText textColor="#E74C3C">
+              {formik.errors.email}
+            </FormHelperText>
+          )}
         </FormControl>
 
         <FormControl id="password" p={4} isRequired>
           <FormLabel>Senha</FormLabel>
-          <Input type="password" />
+          <Input
+            type="password"
+            values={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && (
+            <FormHelperText textColor="#E74C3C">
+              {formik.errors.password}
+            </FormHelperText>
+          )}
         </FormControl>
 
         <Box display="flex" flexDirection="row" alignItems="center">
           <Text>clocker.work/</Text>
           <FormControl id="username" p={4} isRequired>
-            <Input type="username" />
+            <Input
+              type="username"
+              values={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.username && (
+              <FormHelperText textColor="#E74C3C">
+                {formik.errors.username}
+              </FormHelperText>
+            )}
           </FormControl>
         </Box>
-        <Button width="100%" p={4}>Entrar</Button>
+        <Box p={4}>
+          <Button width="100%">Entrar</Button>
+        </Box>
       </Box>
     </Container>
   );
