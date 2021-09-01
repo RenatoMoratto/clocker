4410238974bf259b4ac9364cec8c164db2df80d4
Facesoccer <80411445+RenatoMoratto@users.noreply.github.com>
2021-09-01 09:32:45 -0300

    fix: create time block form

--- components/Input/index.js
+++ components/Input/index.js
@@ -8,7 +8,7 @@ import {
 
 export const Input = ({ error, label, touched, ...props }) => (
   <Box>
-    <FormControl id="password" p={4} isRequired>
+    <FormControl id={props.name} p={4} isRequired>
       <FormLabel>{label}</FormLabel>
       <InputBase {...props} />
       {touched && <FormHelperText textColor="#E74C3C">{error}</FormHelperText>}
